45e7fc7ac0489351cb2ee460ee2108abb966addb
tdsone <mail@timonschneider.de>
2025-10-09 15:19:42 +0200

    Second puzzle

--- 09102025.py
+++ 09102025.py
@@ -16,3 +16,56 @@ def fibonacciModified(t1, t2, n):
         t2 = res
 
     return res
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxSubarray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+"""
+https://www.hackerrank.com/challenges/maxsubarray/problem?isFullScreen=true
+"""
+
+
+def maxSubarray(arr):
+    """
+    Given an array, find the maximum possible sum among:
+
+    - all nonempty subarrays.
+    - all nonempty subsequences.
+
+    Subarray algo is just dp pretending that dp[i] stores the best possible sum that ends with arr[i]
+
+    subsequences.... hm
+    - this is just all positive numbers...
+
+    Solution: we can do it in one for loop i think
+    """
+
+    N = len(arr)
+    dp = [None] * N
+
+    dp[0] = arr[0]
+    max_subseq = arr[0]
+
+    for i in range(1, N):
+        dp[i] = arr[i] if arr[i] > dp[i - 1] + arr[i] else arr[i] + dp[i - 1]
+        if max_subseq < 0 and arr[i] > max_subseq:
+            max_subseq = arr[i]
+        elif max_subseq >= 0 and arr[i] > 0:
+            max_subseq += arr[i]
+
+    max_subarray = max(dp)
+
+    return max_subseq, max_subarray
