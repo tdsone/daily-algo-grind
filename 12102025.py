3c67b572c27ddec6a72f962cdc7bbaf72af059aa
tdsone <mail@timonschneider.de>
2025-10-12 16:13:01 +0200

    Again easy problem

--- 12102025.py
+++ 12102025.py
@@ -22,3 +22,19 @@ class Solution:
 
         print(anagrams)
         return list(anagrams.values())
+
+
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        if strs == []:
+            return None
+        if strs == [""]:
+            return ""
+        return ",".join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        if s == None:
+            return []
+        if s == "":
+            return [""]
+        return s.split(",")
